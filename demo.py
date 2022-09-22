a27edcaf664a5543deec7f19b8b93a0cf5aa69e8
Mohamed <mohamedelsayedelawady@gmail.com>
2022-09-22 11:25:07 +0100

    fix relative imports

--- demo.py
+++ demo.py
@@ -3,20 +3,21 @@ import torch
 import torch.nn
 import torchvision
 import numpy as np
+from PIL import Image
 from datasets import to_rgb, transform_test
 from model import *
-import config as c
-import modules.Unet_common as common
-from PIL import Image
+from modules import Unet_common as common
+# import config as c
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
+channels_in = 3
+device_ids = [0]
 
 def load_model(model_path):
     net = Model()
     net = net.to(device)
     init_model(net, device=device)
-    net = torch.nn.DataParallel(net, device_ids=c.device_ids)
+    net = torch.nn.DataParallel(net, device_ids=device_ids)
     state_dicts = torch.load(model_path, map_location=device)
     network_state_dict = {
         k: v for k, v in state_dicts["net"].items() if "tmp_var" not in k
@@ -42,27 +43,31 @@ def encode_watermark(model_path, cover, secret):
         secret_input = dwt(secret)
         input_img = torch.cat((cover_input, secret_input), 1)
         output = net(input_img)
-        output_steg = output.narrow(1, 0, 4 * c.channels_in)
+        output_steg = output.narrow(1, 0, 4 * channels_in)
         output_z = output.narrow(
-            1, 4 * c.channels_in, output.shape[1] - 4 * c.channels_in
+            1, 4 * channels_in, output.shape[1] - 4 * channels_in
         )
         steg_img = iwt(output_steg)
     return steg_img, output_z
 
 
-def decode_watermark(model_path, steg_img, output_z):
+def decode_watermark(model_path, steg_img, output_z=None):
     net = load_model(model_path)
     net.eval()
     dwt = common.DWT()
     iwt = common.IWT()
-    backward_z = gauss_noise(output_z.shape)
-    print(backward_z.shape)
+    if output_z is not None:
+        z_shape = output_z.shape
+    else:
+        z_shape = (1,12,512,512)
+    backward_z = gauss_noise(z_shape)
+    # print(backward_z.shape)
     output_steg = dwt(steg_img)
     with torch.no_grad():
         output_rev = torch.cat((output_steg, backward_z), 1)
         bacward_img = net(output_rev, rev=True)
         secret_rev = bacward_img.narrow(
-            1, 4 * c.channels_in, bacward_img.shape[1] - 4 * c.channels_in
+            1, 4 * channels_in, bacward_img.shape[1] - 4 * channels_in
         )
         secret_rev = iwt(secret_rev)
     return secret_rev
@@ -107,6 +112,6 @@ if __name__ == "__main__":
     print(output_z.shape)
     print(steg_img.shape)
 
-    secret_rev = decode_watermark(model_path, steg_img, output_z)
+    secret_rev = decode_watermark(model_path, steg_img, output_z=None)
     img_rec = process_out(secret_rev)
     img_rec.save("img_rec.jpg")
